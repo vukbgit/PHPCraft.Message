b89c8a55a2ce2952550cadea5b9c313d1827feac
vukbgit <info@vuk.bg.it>
2016-04-23 17:41:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Store and get application messages into custom categories and onto different supports.
+Currently only implemented support is cookies by means of [PHPCraft.Cookie](https://github.com/vukbgit/PHPCraft.Cookie) and ultimately of [Patrick Louys Http](https://github.com/PatrickLouys/http) but sessions and maybe database are planned
+Categories can be used to build messages display interface, for example using Bootstrap helper message but this class do NOT generate interface, that is view layer business (i.e. template)
+
---
+++ src/Message.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace PHPCraft\Message;
+
+use PHPCraft\Cookie;
+
+/**
+ * Manages application messages storing them onto various supports (currently only cookies implemented)
+ *
+ * @author vuk <info@vuk.bg.it>
+ */
+class Message
+{
+    private $cookieBuilder;
+
+    /**
+     * Constructor.
+     **/
+    public function __construct()
+    {
+    }
+
+    /**
+    * sets optional dependency for cookies support
+    *
+    * @param PHPCraft\Cookie\CookieBuilderInterface $cookieBuilder
+    */
+    public function setCookie(CookieBuilderInterface $cookieBuilder)
+    {
+        $this->cookieBuilder = $cookieBuilder;
+    }
+    
+    /**
+    * saves a message
+    *
+    * @param string $support: support to save message to, so far only 'cookies'
+    * @param string $category: used to index message; for example Bootstrap contextual background helper classes (http://getbootstrap.com/css/#helper-classes-backgrounds) may be used for template benefit 
+    * @throws Exception if $support is 'cookies' and $this->cookieBuilder has not been set
+    * @throws DomainException if $support is not handled
+    */
+    function saveMessage($support,$category,$message)
+    {
+        switch($support) {
+            case 'cookies':
+                if(!$this->cookieBuilder) throw new Exception('cookieBuilder must be set');
+                $messages = (array) json_decode($this->cookieBuilder->get('messages'));
+                if(!$messages) $messages = array();
+                if(!isset($messages[$category])) $messages[$category] = array();
+                $messages[$category][] = $message;
+                $this->cookieBuilder->set('messages', json_encode($messages), false, COOKIE_HTTPONLY);
+            break;
+            default:
+                throw new DomainException(sprintf('Unknown support \'%s\' for message: %s',$support,$message));
+            break;
+        }
+    }
+    
+    /**
+    * gets all of messages and deletes them from support
+    *
+    * @param string $support: support messages are saved to, so far only cookies
+    * @throws DomainException if $support is not handled
+    * @return array of messages indexed by categories
+    */
+    function getMessages($support)
+    {
+        switch($support) {
+            case 'cookies':
+                $messages = (array) json_decode($this->cookieBuilder->get('messages'));
+                $this->cookieBuilder->delete('messages');
+            break;
+            default:
+                throw new DomainException(sprintf('Unknown required support \'%s\' while getting messages',$support));
+            break;
+        }
+        return $messages;
+    }
+}
